46ab18799120cdb73cf18d8d813abe0a56e4bc93
Developer <izaque_arcangelo@hotmail.com>
2017-08-16 20:44:37 -0300

    Fix: Mudanças na configuração do template.

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container">
+        <div class="social-links text-center">
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+        </div>
+        <div class="copy text-center">
+            Copyright 2017 <a href='#'>Curso</a> todos os direitos reservados.
+        </div>
+    </div>
+</footer>
