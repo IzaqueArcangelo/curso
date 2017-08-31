22c78125b56c9693f38c34902f7b03ba6c2d4e0c
Developer <izaque_arcangelo@hotmail.com>
2017-08-31 20:50:02 -0300

    Fix: inclusão de validador de CPF. https://github.com/geekcom/validator-docs

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Cpf Validator
+         */
+        geekcom\ValidatorDocs\ValidatorProvider::class
+
     ],
 
     /*
