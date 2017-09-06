b5a45cf69e39a97b0bade902cf34b8abbb32be1b
Developer <izaque_arcangelo@hotmail.com>
2017-09-06 17:26:26 -0300

    Fix: Correções no modelo Aluno.

--- app/Model/Aluno.php
+++ app/Model/Aluno.php
@@ -9,6 +9,7 @@ class Aluno extends Model
     protected $table = 'aluno'; // TODO especifica o nome da tabela, evitando que o láravel assuma a convenção.
     public $timestamps = false; //TODO remove o gerenciamento automático do láravel created_at e updated_at.
 
+
     public function endereco(){
         return $this->belongsTo('App\Model\Endereco', 'id_endereco', 'id');
     }
